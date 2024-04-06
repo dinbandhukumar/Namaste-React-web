20d28ecf621b42dcfb37515761eb6f31e09d6339
unknown <kumardinbandhu87@gmail.com>
2024-04-06 23:54:25 +0530

    My first commit for Day-1

---
+++ App.js
@@ -0,0 +1,24 @@
+
+const heading = React.createElement(
+"h1",
+
+{id :'heading'},
+
+"Hello world from React library !");
+
+/*
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { key: 1 }, "I am in h1 tag"),
+    React.createElement("h2", { key: 2 }, "I am in h2 tag"),
+  ])
+); 
+
+*/
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
